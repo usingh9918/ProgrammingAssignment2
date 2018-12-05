2e37167eb24067a480dd18ee9e8aedaec68884ea
usingh9918 <usingh9918@gmail.com>
2018-12-05 13:14:24 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Hi! Welcome to my submission of Assignment 2: Lexical Scoping 
 
-## Write a short comment describing this function
+## makeCacheMatrix function calculates the inverse of a valid square matrix,
+
+## and store the result in the Cache memory.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      
+      set <- function(y){
+            
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) m <<- inverse
+      getinverse <- function() m
+      
+      list( set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function retrieves the stored value of the inverse of a the matrix. If the value
+
+## does not exist, the function calculate the inverse and stores the data in the Cache memory.
 
 cacheSolve <- function(x, ...) {
+      
+      m <- x$getinverse()
+      
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+            
+            
+      }
+      data <- x$get()
+      
+      m<- solve(data,...)
+      x$setinverse(m)
         ## Return a matrix that is the inverse of 'x'
 }
